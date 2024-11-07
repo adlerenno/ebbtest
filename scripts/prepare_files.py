e1c5dd8a8e4a757ef3b63530281c865ce6b73103
Enno Adler <eadler@mail.uni-paderborn.de>
2024-11-07 15:15:21 +0100

    #1 Initial test, added datasets, added ibb

--- scripts/prepare_files.py
+++ scripts/prepare_files.py
@@ -22,8 +22,8 @@ def system(cmd: str):
     return t
 
 
-def prepare_files(file: str, multiline: bool, output_basename: str):
-    """Multiline=True means, that there is only a single line per read.
+def prepare_files(file: str, output_basename: str):
+    """Multiline=True means that there is only a single line per read.
     Use multiline=False if a read is divided into multiple lines."""
     # check for .gz file
     if file.endswith('.gz'):
@@ -33,54 +33,45 @@ def prepare_files(file: str, multiline: bool, output_basename: str):
         if not os.path.isfile(file):
             raise FileNotFoundError(f"Input file does not exist: {file}")
 
-    is_fasta = file.split('.')[-1] in {'fa', 'fasta'}
     # check for fasta and create one word per line file
-    line_number = 0
-    written_reads = 0
+    status = 0
+    sequence_number = 0
+    line_lengths = []
     if not os.path.isfile(output_basename + '.owpl'):
         print(f'Preparing file: {file}')
         with open(file) as input_file:
             with open(output_basename + '.owpl', 'w') as output_owpl:  # owpl = one word per line
                 with open(output_basename + '.fa', 'w') as output_fasta:
                     with open(output_basename + '.fq', 'w') as output_fastq:
-                        if not multiline:
-                            output_fasta.write('>\n')
                         for line in input_file:
-                            line = line.upper()
-                            if (multiline and is_fasta and line_number % 2 == 1) \
-                                    or (multiline and not is_fasta and line_number % 4 == 1):
-                                assert line[0] not in {'>', '+', '@', ':', ';', 9, 8}
-                                line = ''.join(filter(lambda x: x in {'A', 'C', 'G', 'T'}, line))
+                            if line.startswith('>'):
+                                output_fasta.write(line)
+                                if status != 0:
+                                    output_fastq.write(f'+ length={len(line)}\n')
+                                    for length in line_lengths:
+                                        output_fastq.write(':' * length + '\n')
+                                    line_lengths.clear()
+                                output_fastq.write(f'@{line[1:]}')
+                                sequence_number += 1
+                                status = 1
+                            elif line.startswith(':'):
+                                status = 2
+                            elif status == 1:
+                                line = ''.join(filter(lambda x: x in {'A', 'C', 'G', 'T'}, line.upper()))
+                                assert len(line) > 0
 
                                 output_owpl.write(line + '\n')
 
-                                output_fasta.write(f'>S{line_number}\n')
                                 output_fasta.write(line + '\n')
 
-                                output_fastq.write(f'@S{line_number} length={len(line)}\n')
                                 output_fastq.write(line + '\n')
-                                output_fastq.write(f'+ length={len(line)}\n')
-                                output_fastq.write(':' * len(line) + '\n')
-                            if not multiline:
-                                assert line[0] not in {'+', '@', ':', ';', 9, 8}
-                                if line[0] == '>':
-                                    output_fasta.write('>\n')
-                                    output_owpl.write('\n')
-                                else:
-                                    line = ''.join(filter(lambda x: x in {'A', 'C', 'G', 'T'}, line))
+                                line_lengths.append(len(line))
 
-                                    output_owpl.write(line)
-
-                                    output_fasta.write(line + '\n')
-
-                                    # HINT: I do not support writing multiline fastq yet. Add id here otherwise.
-                                    # output_fastq.write(f'@ length={len(line)}\n')
-                                    # output_fastq.write(line + '\n')
-                                    # output_fastq.write(f'+ length={len(line)}\n')
-                                    # output_fastq.write(':' * len(line) + '\n')
-                            line_number += 1
-                        if multiline:
-                            output_fasta.write('\n')
+                        if len(line_lengths) > 0:
+                            output_fastq.write(f'+ length={len(line)}\n')
+                            for length in line_lengths:
+                                output_fastq.write(':' * length + '\n')
+                            line_lengths.clear()
                         output_owpl.flush()
                         output_fasta.flush()
                         output_fastq.flush()
